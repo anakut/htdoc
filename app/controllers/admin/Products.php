7e2fa42e5b0ea69ee68dca937df783fee445b4fb
Longsoftware <67464470+Longsoftware@users.noreply.github.com>
2020-07-30 13:45:27 +0700

    change file
    confirm

--- app/controllers/admin/Products.php
+++ app/controllers/admin/Products.php
@@ -80,7 +80,7 @@ class Products extends MY_Controller
         $this->load->library('datatables');
         if ($warehouse_id) {
             $this->datatables
-            ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code, {$this->db->dbprefix('products')}.name as name,{$this->db->dbprefix('products')}.name as name,{$this->db->dbprefix('products')}.test as test, {$this->db->dbprefix('brands')}.name as brand,{$this->db->dbprefix('location')}.name as location, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, COALESCE(wp.quantity, 0) as quantity, {$this->db->dbprefix('units')}.code as unit, wp.rack as rack, alert_quantity", FALSE)
+            ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code, {$this->db->dbprefix('products')}.name as name,{$this->db->dbprefix('products')}.name as name, {$this->db->dbprefix('brands')}.name as brand,{$this->db->dbprefix('location')}.name as location, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, COALESCE(wp.quantity, 0) as quantity, {$this->db->dbprefix('units')}.code as unit, wp.rack as rack, alert_quantity", FALSE)
             ->from('products');
             if ($this->Settings->display_all_products) {
                 $this->datatables->join("( SELECT product_id, quantity, rack from {$this->db->dbprefix('warehouses_products')} WHERE warehouse_id = {$warehouse_id}) wp", 'products.id=wp.product_id', 'left');
@@ -96,7 +96,7 @@ class Products extends MY_Controller
             // ->group_by("products.id");
         } else {
             $this->datatables
-                ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code, {$this->db->dbprefix('products')}.name as name,{$this->db->dbprefix('products')}.test as test, {$this->db->dbprefix('brands')}.name as brand,{$this->db->dbprefix('location')}.name as location, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, COALESCE(quantity, 0) as quantity, {$this->db->dbprefix('units')}.code as unit, '' as rack, alert_quantity", FALSE)
+                ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code, {$this->db->dbprefix('products')}.name as name, {$this->db->dbprefix('brands')}.name as brand,{$this->db->dbprefix('location')}.name as location, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, COALESCE(quantity, 0) as quantity, {$this->db->dbprefix('units')}.code as unit, '' as rack, alert_quantity", FALSE)
                 ->from('products')
                 ->join('categories', 'products.category_id=categories.id', 'left')
                 ->join('units', 'products.unit=units.id', 'left')
@@ -150,6 +150,7 @@ class Products extends MY_Controller
             $this->data['rack'] = $wh_pr['rack'];
             $this->data['modal_js'] = $this->site->modal_js();
             $this->load->view($this->theme . 'products/set_rack', $this->data);
+
         }
     }
 
@@ -363,8 +364,6 @@ class Products extends MY_Controller
                 'code' => $this->input->post('code'),
                 'barcode_symbology' => $this->input->post('barcode_symbology'),
                 'name' => $this->input->post('name'),
-                //add to product               
-                'test' => $this->input->post('test'),
                 'type' => $this->input->post('type'),
                 'brand' => $this->input->post('brand'),
                 'location' => $this->input->post('location'),
@@ -783,10 +782,6 @@ class Products extends MY_Controller
                 'barcode_symbology' => $this->input->post('barcode_symbology'),
                 'name' => $this->input->post('name'),
                 'type' => $this->input->post('type'),
-//<!-- edit  -->
-                'test' => $this->input->post('test'),
-
-
                 'brand' => $this->input->post('brand'),
                 'location' => $this->input->post('location'),
                 'category_id' => $this->input->post('category'),
@@ -1090,10 +1085,6 @@ class Products extends MY_Controller
 
     function import_csv()
     {
-                                                                                     
-        $warehouse_qty=null;
-                                                                                    
-        $code = null;
         $this->sma->checkPermissions('csv');
         $this->load->helper('security');
         $this->form_validation->set_rules('userfile', lang("upload_file"), 'xss_clean');
@@ -1135,12 +1126,14 @@ class Products extends MY_Controller
                     $final[] = array_combine($keys, $value);
                 }
                 // $this->sma->print_arrays($final);
-                $rw = 2; $data = array();
+                $rw = 2; $items = array();
                 foreach ($final as $csv_pr) {
                     if ( ! $this->products_model->getProductByCode(trim($csv_pr['code']))) {
                         if ($catd = $this->products_model->getCategoryByCode(trim($csv_pr['category_code']))) {
                             $brand = $this->products_model->getBrandByName(trim($csv_pr['brand']));
                             $location = $this->products_model->getLocationByName(trim($csv_pr['location']));
+                            $warehouse = $this->products_model->getWarehouseByName(trim($csv_pr['cf1']));
+                           
                             $unit = $this->products_model->getUnitByCode(trim($csv_pr['unit']));
                             $base_unit = $unit ? $unit->id : NULL;
                             $sale_unit = $base_unit;
@@ -1162,7 +1155,6 @@ class Products extends MY_Controller
 
                             $tax_details = $this->products_model->getTaxRateByName(trim($csv_pr['tax_rate']));
                             $prsubcat = $this->products_model->getCategoryByCode(trim($csv_pr['subcategory_code']));
-                        
                             $items[] = array (
                                 'code' => trim($csv_pr['code']),
                                 'name' => trim($csv_pr['name']),
@@ -1181,23 +1173,21 @@ class Products extends MY_Controller
                                 'subcategory_id' => ($prsubcat ? $prsubcat->id : NULL),
                                 'variants' => trim($csv_pr['variants']),
                                 'image' => trim($csv_pr['image']),
-                                'warehouse' => trim($csv_pr['cf1']),
+                                'warehouse' => trim($warehouse ? $warehouse->id : NULL),
                                 'quantity' => trim($csv_pr['cf2']),
-                                'cf3' => trim($csv_pr['cf3']),
-                                'cf4' => trim($csv_pr['cf4']),
-                                'cf5' => trim($csv_pr['cf5']),
-                                'cf6' => trim($csv_pr['cf6']),
+                                'cf1' => trim($csv_pr['cf3']),
+                                'cf2' => trim($csv_pr['cf4']),
+                                'cf3' => trim($csv_pr['cf5']),
+                                'cf4' => trim($csv_pr['cf6']),
+                                
+                               
                                 );
-
-                             
-                            
-                            
                         } else {
                             $this->session->set_flashdata('error', lang("check_category_code") . " (" . $csv_pr['category_code'] . "). " . lang("category_code_x_exist") . " " . lang("line_no") . " " . $rw);
                             admin_redirect("products/import_csv");
-                          }
+                        }
                     }
-              
+
                     $rw++;
                 }
             }
@@ -1205,19 +1195,10 @@ class Products extends MY_Controller
             // $this->sma->print_arrays($items);
         }
 
-       
-
-
-
-     //   if ($this->form_validation->run() == true && $prs = $this->products_model-> addProduct($data, $items,$saleUnit, $warehouse_qty, $product_attributes, $photos)) {
-     //       $this->session->set_flashdata('message', sprintf(lang("products_added"), $prs));
-    //        admin_redirect('products');
-      //  } 
-       if ($this->form_validation->run() == true && $prs = $this->products_model->add_products($items)) {
-         $this->session->set_flashdata('message', sprintf(lang("products_added"), $prs));
+        if ($this->form_validation->run() == true && $prs = $this->products_model->add_products($items)) {
+            $this->session->set_flashdata('message', sprintf(lang("products_added"), $prs));
             admin_redirect('products');
-        } 
-        else {
+        } else {
 
             $this->data['error'] = (validation_errors() ? validation_errors() : $this->session->flashdata('error'));
 
@@ -2499,5 +2480,5 @@ class Products extends MY_Controller
         }
 
     }
-    
+
 }
